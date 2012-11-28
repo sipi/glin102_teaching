55e13630fec8ebb516c8bb7b0a94900b2b8a5b2a
Clément SIPIETER <c.sipieter@gmail.com>
2012-11-28 17:13:54 +0100

    v1a

--- teaching.php
+++ teaching.php
@@ -6,7 +6,7 @@
   <h2>GLIN102 - Concepts de base en informatique</h2>
   <h3>Ma première page web</h3>
   
-  <p>Réalisez par groupe de deux ou trois, un site web de 2 pages sur le sujet de votre choix (de préférence un truc qui vous branche). 
+  <p>Réalisez par groupe de deux ou trois, un site web de 2 pages sur le sujet de votre choix (de préférence un truc qui vous branche).</p>
   
   <h4>What is HTML?</h4>
   <p><cite>L’Hypertext Markup Language, généralement abrégé HTML, est le format 
@@ -19,9 +19,10 @@
   variés de manière conforme aux exigences de l’accessibilité du web. Il 
   est souvent utilisé conjointement avec des langages de programmation 
   (JavaScript) et des formats de présentation (feuilles de style en cascade). 
-  HTML est initialement dérivé du Standard Generalized Markup Language (SGML).</cite> <span class="cite_source" >Wikipedia</span> </p>
+  HTML est initialement dérivé du Standard Generalized Markup Language (SGML).</cite> <span class="cite_source" >[Wikipedia 2012]</span> </p>
   
-  <img src="../img/structure_document_html.png" alt="Structure d'un document HTML"/>
+  <img id="img_struct_doc_html" class="center" src="../img/structure_document_html.png" alt="Structure d'un document HTML"/>
+  <div class="legend" >Structure d'un document HTML [Wikipedia 2012]</div>
   
   <h4>Les outils</h4>
     <ul>
@@ -34,7 +35,7 @@
       </li>
       <li>
       Un éditeur de texte : kate (emacs, vim pour les fanatiques)
-      <span onClick="toggle_aid('aid_traitement_texte')" class="view_aid" href="">Attention, un éditeur de texte n'est pas un traitement de text</span>
+      <span onClick="toggle_aid('aid_traitement_texte')" class="view_aid" href="">Attention, un éditeur de texte n'est pas un traitement de texte</span>
       <div id="aid_traitement_texte" class="aid">
         Un traitement de texte permet la mise en forme d'un document 
         (taille et couleur de police, insertion d'images…).<br/>
@@ -47,7 +48,7 @@
   
   <h4>Ce que vous devez savoir faire après ce TP</h4>
   
-  Votre site internet devra contenir les élements suivant :</p>
+  <p>Votre site internet devra contenir les élements suivant :</p>
   
   <ul>
     <li>h1, h2, h3,…</li>
@@ -69,19 +70,38 @@
   
   <h4>Dans un deuxième temps</h4>
     <ul>
-      <li>Les attributs css: background-image, float</li>
-      <li>Regardez comment modifier un élement particulier grâce aux attributs id et class des balises html et comment les modifier avec du css</li>
+      <li>
+        Associer un fichier css à votre site web 
+        <span onClick="toggle_aid('aid_link_css')" class="view_aid" href="">help?</span>
+        <div id="aid_link_css" class="aid"> 
+          Inserer la balise suivante dans l'entête de votre page web. <br/>
+          &lt;link rel="stylesheet" type="text/css" href="style.css" /&gt; <br/>
+          Vous pouvez utiliser <a href="../downloads/style.css" >ce fichier css d'exemple</a>.
+        </div>
+      </li>
+      <li>Regarder les attributs css suivants : <em>color, font, text-align, margin, background-image, float, etc…</em></li>
+      <li>Regarder les attributs id et class des balises html et comment y faire référence depuis un fichier css
+      <span onClick="toggle_aid('aid_id_class')" class="view_aid" href="">help?</span>
+        <div id="aid_id_class" class="aid"> 
+          <ul>
+            <li><a href="http://css.mammouthland.net/selecteurs-css-class-id.php" target="_blank" >Sélecteurs CSS class et id</a></li>
+            <li><a href="http://www.w3schools.com/css/css_id_class.asp" target="_blank" >w3schools.com</a></li>
+          </ul>
+        </div>
+      </li> 
       <li>Utilisation de Open Office pour créer des pages Web</li>
     </ul>
   
   <h4>Best practices</h4>
-    <dl>
-      <dt>Indentation</dt><dd></dd>
-      <dt>Sépartion fond (HTML) / forme (CSS)</dt><dd></dd>
-  </dl>
+    
+    <ul>
+      <li><a href="http://net.tutsplus.com/tutorials/html-css-techniques/30-html-best-practices-for-beginners/" target="_blank">30 HTML Best Practices for Beginners</a></li>
+      <li><a href="http://fr.wikipedia.org/wiki/Style_d%27indentation" target="_blank">Indenter</a> votre code</li>
+      <li>Sépartion fond (HTML) / forme (CSS) <!-- @todo link needed --></li>
+  </ul>
   
   
-  <h4>ressources diverses</h4>
+  <h4>Ressources diverses</h4>
     <ul>
       <li><a href="http://www.siteduzero.com/tutoriel-3-13666-apprenez-a-creer-votre-site-web-avec-html5-et-css3.html" target="_blank">www.siteduzero.com</a></li>
       <li><a href="http://www.w3schools.com/" target="_blank">www.w3schools.com</a></li>
@@ -90,13 +110,16 @@
       <li><a href="http://duckduckgo.com/" target="_blank">Votre moteur de recherche préféré</a></li>
     </ul>
     
-  <h5>Pour allez plus loin</h5>
+  <h4>Pour allez plus loin</h4>
     <ul>
       <li>javascript</li>
       <li>HTML5/css3</li>
       <li>php/mysql</li>
       <li>xml</li>
     </ul>
+    
+  <h4>Auteurs</h4>
+    <p>Walid BENGHARIT, Clément SIPIETER</p>
     
   <script type="text/javascript">
     var elements = document.getElementsByClassName("aid");
