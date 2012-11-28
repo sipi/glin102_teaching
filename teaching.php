411a7ebc3a7940197020668c20203dc592cc3e64
Clément SIPIETER <c.sipieter@gmail.com>
2012-11-28 11:14:48 +0100

    first commit

---
+++ teaching.php
@@ -0,0 +1,34 @@
+
+<div id="teaching">
+
+  <h1>Enseignements</h1>
+
+  <h2>GLIN102 - Concepts de base en informatique</h2>
+  <h3>Ma première page web</h3>
+  
+  <p>Réalisez par groupe de deux ou trois, un site web sur le sujet de votre choix. Vous pouvez utilisez ce squelette de page vierge. Votre site internet devra contenir les élements suivant :</p>
+  
+  <ul>
+    <li>h1, h2, h3,…</li>
+    <li>p, a, img, br</li>
+    <li>em, strong</li>
+    <li>ul, ol</li>
+    <li>table</li>
+    <li>des <i>&lt;!-- commentaires --&gt;</i></li>
+  </ul>
+  
+  <p>Vous trouverez des informations sur ces <em>balises</em> à cette adresse : <a href="http://www.w3schools.com/tags/default.asp" >www.w3schools.com</a>. Vous pouvez aussi consulter le code source d'un site web existant pour vous en inspirer.<a id="view_aid_view_source" class="view_aid" href=""></a></p>
+  
+  <div id="aid_view_source" class="aid">clique droit sur une page web -&gt; Afficher le code source de la page</div>
+  
+  <script type="text/javascript">
+
+    var elements = document.getElementsByClassName("aid");
+    for (var i in elements)
+      {
+        var e = elements[i];
+        e.style.visibility="hidden";
+      }
+  </script>
+  
+</div>
